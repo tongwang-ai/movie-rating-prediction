aabeb153aa3cf42bcd6b8b3d3cc8e9df07798f51
Tong Wang <wangtongada@gmail.com>
2025-04-21 10:41:48 -0400

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("My First APP")
 
-name = st.text_area("What's your name?")
+name = st.text_area("What's your name?") # or use st.text_input for a smaller text input box (one line)
 
 if name:
   st.write(f"Hello, {name}!")
